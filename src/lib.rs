3350bb8ff3dbad47d9fde20179f26d2fa4ea4993
Craig Disselkoen <craigdissel@gmail.com>
2020-01-18 12:25:25 -0800

    add html_root_url attribute

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+// this ensures that crate users generating docs with --no-deps will still
+// properly get links to the public docs for boolector's types
+#![doc(html_root_url = "https://docs.rs/boolector/0.1.2")]
+
 mod btor;
 pub use btor::{Btor, SolverResult};
 mod node;
